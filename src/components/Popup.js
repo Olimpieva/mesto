b971fc2551bcbc6371f49f1baf7f30c3cc32c1c2
Natalya Olimpieva <ukurennyi.koshak@gmail.com>
2021-08-30 17:44:57 +0300

    refactor(popup): method open

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -6,14 +6,13 @@ export default class Popup {
 
     open() {
         this._popup.classList.add('popup_is-opened');
-        this._popup.classList.add('popup_animated'); // class "popup_animated" was added to solve the problem with the popup flickering when rendering the page. I don't want to remove it, please.
+        this._popup.classList.add('popup_animated');
         this._popup.addEventListener('click', this._handleOverlayClose);
         document.addEventListener('keyup', this._handleEscClose);
     }
 
     close() {
         this._popup.classList.remove('popup_is-opened');
-        // this._popup.classList.remove('popup_animated');
         this._popup.removeEventListener('click', this._handleOverlayClose);
         document.removeEventListener('keyup', this._handleEscClose);
     }
